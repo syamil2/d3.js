c05e7d6a699e140bbc801b7e3654d3ec4393b71d
Syamil <49608148+syamil2@users.noreply.github.com>
2019-04-14 18:08:05 +0800

    Update tableau.js

--- tableau.js
+++ tableau.js
@@ -1,1 +1,6 @@
+function initViz() {
+    var containerDiv = document.getElementById("tableauViz"),
+    url = "https://YOUR-SERVER/views/YOUR-VISUALIZATION";
 
+    var viz = new tableau.Viz(containerDiv, url);
+}
